48b5e0e93e1e644b28fe558da00151a2f0672843
BudhirajaChinmay <chinmay17274@iiitd.ac.in>
2019-03-02 16:11:17 +0530

    Test the app.post

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,52 @@
+// VARIABLES AND CONSTANTS
+const express = require("express");
+var mysql = require('mysql');
+const bodyparser = require('body-parser');
+const Route = require('./Routes');
+
+const app = express();
+const port = 8000;
+
+// Input by the user
+var source,destination;
+
+// Current Day and Date 
+var date = new Date();
+var intday = date.getDay();
+var day;
+
+// Variable to store the stops encountered
+var stops = [];
+
+if(intday == 0){
+    day = "Sunday";
+}
+else if(intday == 1){
+    day = "Monday";
+}
+else if(intday == 2){
+    day = "Tuesday";
+}
+else if(intday == 3){
+    day = "Wedneday";
+}
+else if(intday == 4){
+    day = "Thursday";
+}
+else if(intday == 5){
+    day = "Friday";
+}
+else{
+    day = "Sunday";
+}
+
+// Establishing connection to the database
+var con = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'goodluckall',
+    database : 'my_db'
+  });
 
 /*
 
@@ -7,7 +56,6 @@
 
 // Query from the database
 function Database(con, source, destination){
-
     con.connect()
 
 
@@ -24,37 +72,26 @@ function Database(con, source, destination){
 
 
     // Intersection of all the busses at Source and Destination given by user
-    con.query("SHOW TABLES", function(err,result,fields){
 
-        if(err) throw err;
-
-        var output = result;
-
-        var BusStop = [];
-        for(var i = 0 ; i < result.length ; i++){
-            BusStop.push(result[i].Tables_in_my_db);
-        }
-
-        //console.log(BusStop);
-
-        // Buses from source 
-        var buses_from_source = [];
-        for(var i = 0; i < BusStop.length ; i ++){
-            if(BusStop[i].includes(source)){
-                buses_from_source.push(BusStop[i].substr(0,BusStop[i].indexOf('S')));
+    var buses_from_source = [];
+        for(var key in Route)
+        {
+            
+            if(key.includes(source))
+            {
+                buses_from_source.push(key); 
             }
         }
 
-        // Buses from destination
         var buses_from_destination = [];
-        for(var i = 0; i < BusStop.length ; i++){
-            if(BusStop[i].includes(destination)){
-                buses_from_destination.push(BusStop[i].substr(0,BusStop[i].indexOf('S')));
+        for(var key in Route)
+        {
+            
+            if(key.includes(destination))
+            {
+                buses_from_destination.push(key); 
             }
         }
-
-
-        // Busses from sourse to destination
         var required_buses = [];
         for(var i = 0; i < buses_from_source.length ; i++){
             if(buses_from_destination.includes(buses_from_source[i])){
@@ -62,38 +99,44 @@ function Database(con, source, destination){
             }
         }
 
-        // Route calculation
-        var routes = [];        // Dictionary for storing routes corresponding to each bus
-        var temproute = [];     // Temporary route to assign value in the dictionary
-       
-        var flag_push = false;
-        // for(var i = 0 ; i < required_buses.length ; i++){
 
-        //     if(required_buses)
-        // }
-        //console.log(required_buses);
-        something();
-    });
+        /*  
+            ** IN CASE IF WE DON'T GET THE ROUTES FROM GOOGLE MAP **
 
-    con.query("SELECT Monday FROM B1S1 where TOA = \"2:00:00\" ", function (err, result, fields) {
-        if (err) throw err; 
+            // Route calculation
+            var routes = [];        // Dictionary for storing routes corresponding to each bus
+            var temproute = [];     // Temporary route to assign value in the dictionary
+            
+            var flag_push = false;
+            // for(var i = 0 ; i < required_buses.length ; i++){
+    
+            //     if(required_buses)
+            // }
+            //console.log(required_buses);
+            something();
+         */
+    
 
-        // output of query is an array
-        // array is the dictionaries of that particular time
-        // where key is the day of the week
-        var output=result;
-        var temparray = output[0].Monday.split(",");
+         // Temporary query
+    // con.query("SELECT Monday FROM B1S1 where TOA = \"2:00:00\" ", function (err, result, fields) {
+    //     if (err) throw err; 
 
-        // Assigning integer calues in the data array
+    //     // output of query is an array
+    //     // array is the dictionaries of that particular time
+    //     // where key is the day of the week
+    //     var output=result;
+    //     var temparray = output[0].Monday.split(",");
 
-        var datatemp = [];
-        for(var i = 0 ; i < temparray.length ; i++){
-            datatemp.push(parseInt(temparray[i]));
-        }
+    //     // Assigning integer calues in the data array
 
-        //console.log(datatemp);
-        // TODO :- ** CALL RANGE FUNCTION **
-    });
+    //     var datatemp = [];
+    //     for(var i = 0 ; i < temparray.length ; i++){
+    //         datatemp.push(parseInt(temparray[i]));
+    //     }
+
+    //     //console.log(datatemp);
+    //     // TODO :- ** CALL RANGE FUNCTION **
+    // });
 
     //console.log(datatemp);
     con.end()
@@ -102,7 +145,14 @@ function Database(con, source, destination){
 
 // Algorithm to give the range for seats
 
-function RangeOfSeats(data) {
+function RangeOfSeats(data1) {
+
+    var data = [];
+
+    for(var i = 0 ; i < data1.length ; i++ ){
+        data.push(parseInteger(data1[i]));
+    }
+
     var Slopes=[];
 
     for(var i = 0; i < data.length-1; i++) {
@@ -135,37 +185,60 @@ function RangeOfSeats(data) {
 
 }
 
-
 /*
-
-        MAIN CODE 
+    END OF FUNCTIONS
 
 */
 
-const express = require("express");
-var mysql = require('mysql');
-const Route = require('./Routes');
+app.use(bodyparser.urlencoded({extended: false}));
 
-const app = express();
-const port = 8000;
+// HTML DOCUMENT FOR THE CUSTOMER AND SAVE THE DATA IN DATABASE
+// ******** TEST THE app.post ********
+app.post('/',function(request,response){
 
-// Input by the user
-var source,destination;
+    con.connect();
+    var BusNumber = request.body.user.busnumber.toString(10);
+    var timeofArrival  = request.body.user.toa;
+    var Boarding  = request.body.user.boarded.toString(10);
+    var Deboarding = request.body.user.deboarded.toString(10);
 
+    //  **TODO :- Add the value of the current stop** 
+    var currentstop ;
 
-// Code for gmaps and other stufff
-/*
+    // Old valuen to update the new value
+    con.query("SELECT " + day + " FROM " + BusNumber + currentstop + " where + TOA = " + timeofArrival, function (err, result, fields) {
+        if (err) throw err;
+        var entry = result;
+        var seatsAtPreviosStop;
+        
+        if(stops.length == 0){
+            seatsAtPreviosStop = 0;
+        }
+        else{
+            seatsAtPreviosStop = stops[stops.length-1];
+        }
 
-2
+        var currentSeats = Boarding - Deboarding + seatsAtPreviosStop;
+        stops.push(currentSeats);
 
-*/
+        var temp = entry[0].Monday.split(":");
+        var data = temp[0].split(",");
+        data.push(currentSeats.toString(10));
 
-// Establishing connection to the database
-var con = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'goodluckall',
-  database : 'my_db'
+        var updatedrange = RangeOfSeats(data);
+        var range = updatedrange[0].toString(10) + ":" + updatedrange[1].toString(10);
+
+        var updateddatapoint = data+","+range;
+        // Update the value
+
+        con.query("UPDATE " + BusNumber + currentstop + " SET "+ day + " = " + updateddatapoint + " where TOA = " + timeofArrival, function (err, result, fields) {
+            if (err) throw err;
+        });
+
+    });
+
+    con.end();
+
 });
 
 // Query from the database 
